46f095b4801a844e15754544ad1b6cc00b9f1945
Iqbalarsa <iqbalarsa99@gmail.com>
2024-05-14 15:20:36 +0700

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,55 @@
+from flask import Flask, render_template, request, jsonify
+import pandas as pd
+import networkx as nx
+
+app = Flask(__name__)
+
+def read_data():
+    edges_file_path = 'jarakdesa_test1.xlsx'
+    master_desa_file_path = 'master_desa.xlsx'
+    edges_df = pd.read_excel(edges_file_path)
+    master_desa_df = pd.read_excel(master_desa_file_path)
+    return edges_df, master_desa_df
+
+def calculate_shortest_path(edges_df, desa_df, source_desa, target_desa):
+    merged_df1 = pd.merge(edges_df, desa_df, left_on='idDesa1', right_on='iddesa')
+    merged_df2 = pd.merge(merged_df1, desa_df, left_on='idDesa2', right_on='iddesa', suffixes=('_desa1', '_desa2'))
+
+    G = nx.Graph()
+    for index, row in merged_df2.iterrows():
+        G.add_edge(row['nmdesa_desa1'], row['nmdesa_desa2'], jarak=row['jarak'])
+
+    shortest_path_length = nx.dijkstra_path_length(G, source_desa, target_desa, weight='jarak')
+    shortest_path = nx.shortest_path(G, source_desa, target_desa, weight='jarak')
+    return shortest_path, shortest_path_length
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        source_kec = request.form['source_kec']
+        source_desa = request.form['source_desa']
+        target_kec = request.form['target_kec']
+        target_desa = request.form['target_desa']
+        
+        edges_df, master_desa_df = read_data()
+        
+        shortest_path, shortest_path_length = calculate_shortest_path(edges_df, master_desa_df, source_desa, target_desa)
+        
+        return render_template('result.html', source_desa=source_desa, target_desa=target_desa,
+                               shortest_path=shortest_path, shortest_path_length=shortest_path_length)
+    
+    master_desa_df = pd.read_excel('master_desa.xlsx')
+    kecamatan_names = master_desa_df['nmkec'].unique().tolist()
+    
+    return render_template('index.html', kecamatan_names=kecamatan_names)
+
+@app.route('/get_desa', methods=['POST'])
+def get_desa():
+    data = request.get_json()
+    kecamatan = data['kecamatan']
+    master_desa_df = pd.read_excel('master_desa.xlsx')
+    desa_list = master_desa_df.loc[master_desa_df['nmkec'] == kecamatan, 'nmdesa'].tolist()
+    return jsonify({'desa_list': desa_list})
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
